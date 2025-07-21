167269cc3df1cdb3b31baaa5fa38473b9d8f631b
Chan-d-an <ck5172857@gmail.com>
2025-07-21 18:28:14 +0530

    add pwa feature and change home page ui

--- next.config.ts
+++ next.config.ts
@@ -1,36 +1,46 @@
-import type { NextConfig } from "next";
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
       },
       {
-        protocol: "https",
-        hostname: "placehold.net",
+        protocol: 'https',
+        hostname: 'placehold.net',
       },
       {
-        protocol: "https",
-        hostname: "placehold.co",
+        protocol: 'https',
+        hostname: 'placehold.co',
       },
       {
-        protocol: "https",
-        hostname: "cdn.pixabay.com",
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com',
       },
       {
-        protocol: "https",
-        hostname: "images.unsplash.com",
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
       },
       {
-        protocol: "https",
-        hostname: "images.pexels.com",
+        protocol: 'https',
+        hostname: 'images.pexels.com',
       },
     ],
+    domains: [
+      'images.pexels.com',
+      'images.unsplash.com',
+      'lh3.googleusercontent.com',
+      'placehold.net',
+      'placehold.co',
+      'cdn.pixabay.com',
+    ],
   },
-  
 };
-export default nextConfig;
 
+module.exports = withPWA(nextConfig);
